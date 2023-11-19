33ff6d179a0ea18f073f97896b681d16a8f576af
Jorinhe <150986983+Jorinhe@users.noreply.github.com>
2023-11-19 22:50:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,186 @@
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    /*remove the attribute, and call this function once more:*/
+
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+
+                    // -------------------------------------------------------------------------
+
+                    const modal = document.getElementById("modal")
+                    const btnModal = document.getElementById("btn-modal")
+                    btnModal.addEventListener("click", e => {
+                        modal.style.display = "flex"
+                    })
+                    const closeBtn = modal.querySelector(".close-area")
+                    closeBtn.addEventListener("click", e => {
+                        modal.style.display = "none"
+                    })
+
+                    // -------------------------------------------------------------------------
+
+                    // ---------------------------------
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            // -------------------------------------------------------------------------
+            // -------------------------------------------------------------------------
+            return;
+        }
+    }
+};
+
+
+var datas = ['ZANA', 'Microcare', 'MODENA', 'COORDINATING PRODUCTS']
+
+var mnswiper = new Swiper(".mn-sl", {
+    loop: true,
+    slidesPerView: 1,
+    loopedSlides: 1,
+    spaceBetween: 0,
+    loopAdditionalSlides: 2,
+    loopPreventsSlide: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '" style="margin: 0 30px">' + datas[index] + "</span>";
+        },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    speed: 600,
+    autoplay: {
+        delay: 3800,
+        disableOnInteraction: false,
+    },
+
+});
+
+
+$(document).ready(function () {
+    $('#nav-icon3').click(function () {
+        $(this).toggleClass('open');
+    });
+});
+
+function show() {
+    document.getElementById('sidebar').classList.toggle('act');
+}
+
+_open.addEventListener("click", function () {
+    document.body.style.overflow = "hidden";
+    mobileSidebar.style.width = "100%";
+});
+
+_close.addEventListener("click", function () {
+    mobileSidebar.style.width = "0%";
+    document.body.style.removeProperty('overflow');
+});
+
+
+$(document).ready(function () {
+    $("#cbx_chkAll").click(function () {
+        if ($("#cbx_chkAll").is(":checked")) $("input[name=chk]").prop("checked", true);
+        else $("input[name=chk]").prop("checked", false);
+    });
+
+    $("input[name=chk]").click(function () {
+        var total = $("input[name=chk]").length;
+        var checked = $("input[name=chk]:checked").length;
+
+        if (total != checked) $("#cbx_chkAll").prop("checked", false);
+        else $("#cbx_chkAll").prop("checked", true);
+    });
+});
+
+$(".selector").flatpickr({
+    "disable": [
+        function (date) {
+            // return true to disable
+            return (date.getDay() === 0 || date.getDay() === 6);
+
+        }
+    ],
+    "locale": {
+        "firstDayOfWeek": 1 // start week on Monday
+    }
+});
+
+function sample4_execDaumPostcode() {
+    new daum.Postcode({
+        oncomplete: function (data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+            // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
+            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+            var roadAddr = data.roadAddress; // 도로명 주소 변수
+            var extraRoadAddr = ''; // 참고 항목 변수
+
+            // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+            // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+            if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+                extraRoadAddr += data.bname;
+            }
+            // 건물명이 있고, 공동주택일 경우 추가한다.
+            if (data.buildingName !== '' && data.apartment === 'Y') {
+                extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+            }
+            // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+            if (extraRoadAddr !== '') {
+                extraRoadAddr = ' (' + extraRoadAddr + ')';
+            }
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            document.getElementById('sample4_postcode').value = data.zonecode;
+            document.getElementById("sample4_roadAddress").value = roadAddr;
+            document.getElementById("sample4_jibunAddress").value = data.jibunAddress;
+
+            // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
+            if (roadAddr !== '') {
+                document.getElementById("sample4_extraAddress").value = extraRoadAddr;
+            } else {
+                document.getElementById("sample4_extraAddress").value = '';
+            }
+
+            var guideTextBox = document.getElementById("guide");
+            // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
+            if (data.autoRoadAddress) {
+                var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
+                guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
+                guideTextBox.style.display = 'block';
+
+            } else if (data.autoJibunAddress) {
+                var expJibunAddr = data.autoJibunAddress;
+                guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
+                guideTextBox.style.display = 'block';
+            } else {
+                guideTextBox.innerHTML = '';
+                guideTextBox.style.display = 'none';
+            }
+        }
+    }).open();
+}
+
+
